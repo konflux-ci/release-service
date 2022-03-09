99972c9aaa35162cba87f389122054dd554a7123
David Moreno García <damoreno@redhat.com>
2022-03-09 21:46:50 +0100

    Add ReleaseStrategy CRD

--- main.go
+++ main.go
@@ -30,6 +30,8 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	appstudiov1alpha1 "github.com/redhat-appstudio/release-service/api/v1alpha1"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +43,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(appstudiov1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
