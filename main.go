3a09c62165d563d6db0f1d9d4f09e2fa5350f5c9
David Moreno García <damoreno@redhat.com>
2022-03-09 21:51:30 +0100

    Add Release controller skeleton

--- main.go
+++ main.go
@@ -32,6 +32,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	appstudiov1alpha1 "github.com/redhat-appstudio/release-service/api/v1alpha1"
+	"github.com/redhat-appstudio/release-service/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -77,6 +78,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.ReleaseReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Release")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
