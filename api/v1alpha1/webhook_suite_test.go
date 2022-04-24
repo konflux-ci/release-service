82a51aefb84583aec63854a5972ec196986bfa51
David Moreno García <damoreno@redhat.com>
2022-04-24 15:16:58 +0200

    Add release webhook
    The new webhook will forbid updates to Releases as these resources
    should not be updated once created. At that point the controller is
    already processing them and adding data specific to the fields in the
    resource specification.

--- api/v1alpha1/webhook_suite_test.go
+++ api/v1alpha1/webhook_suite_test.go
@@ -80,6 +80,9 @@ var _ = BeforeSuite(func() {
 	err = admissionv1beta1.AddToScheme(scheme)
 	Expect(err).NotTo(HaveOccurred())
 
+	err = admissionv1beta1.AddToScheme(scheme)
+	Expect(err).NotTo(HaveOccurred())
+
 	//+kubebuilder:scaffold:scheme
 
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme})
@@ -99,6 +102,9 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 
 	err = (&ReleaseLink{}).SetupWebhookWithManager(mgr)
+	Expect(err).NotTo(HaveOccurred())
+
+	err = (&Release{}).SetupWebhookWithManager(mgr)
 	Expect(err).NotTo(HaveOccurred())
 
 	//+kubebuilder:scaffold:webhook
