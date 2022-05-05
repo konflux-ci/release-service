64b53b839e952f1e3fdf0b61550dbde76f565787
Johnny Bieren <jbieren@redhat.com>
2022-05-04 20:16:31 -0400

    feat(HACBS-390): Create mock ApplicationSnapshot CRD
    Create an ApplicationSnapshot CRD to unblock us for now. This commit
    is meant to be reverted at a later date once the Gitops team provides
    an ApplicationSnapshot CRD.
    
    Signed-off-by: Johnny Bieren <jbieren@redhat.com>

---
+++ api/v1alpha1/applicationsnapshot_types.go
@@ -0,0 +1,67 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Images defines the various component images associated with the ApplicationSnapshot
+type Image struct {
+	// Component is the component name of the image
+	// +required
+	Component string `json:"component"`
+
+	// PullSpec is the particular version of the component's container image
+	// +required
+	PullSpec string `json:"pullSpec"`
+}
+
+// ApplicationSnapshotSpec defines the desired state of ApplicationSnapshot
+type ApplicationSnapshotSpec struct {
+	// Images is the list of component images that makes up the snapshot
+	// +required
+	Images []Image `json:"images"`
+}
+
+type ApplicationSnapshotStatus struct {
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// ApplicationSnapshot is the Schema for the applicationsnapshots API
+type ApplicationSnapshot struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ApplicationSnapshotSpec   `json:"spec,omitempty"`
+	Status ApplicationSnapshotStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ApplicationSnapshotList contains a list of ApplicationSnapshot
+type ApplicationSnapshotList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ApplicationSnapshot `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ApplicationSnapshot{}, &ApplicationSnapshotList{})
+}
