dbcdf5e2d5162002c81b85b4e33374e5a1b9d6c5
Leandro Mendes <lmendes@redhat.com>
2022-06-09 16:37:00 +0200

    - adding tests to PipelineRun - small adjust - adding tekton_suite_test.go and more changing tests to create resources in the cluster - signing commits - addding PipelinRun creation on cluster before running tests - signing commit

---
+++ tekton/pipeline_run_test.go
@@ -0,0 +1,135 @@
+/**
+Copyright 2022 Red Hat Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package tekton
+
+import (
+	"context"
+	"reflect"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/redhat-appstudio/release-service/api/v1alpha1"
+	tektonv1beta1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type ExtraParams struct {
+	Name  string
+	Value tektonv1beta1.ArrayOrString
+}
+type ReleasePipelineParams struct {
+	Prefix    string
+	Namespace string
+	Extra     ExtraParams
+}
+
+var _ = Describe("PipelineRun", func() {
+
+	var params *ReleasePipelineParams
+	var rplr *ReleasePipelineRun
+
+	var release *v1alpha1.Release
+	var releaseParams *v1alpha1.ReleaseSpec
+
+	BeforeEach(func() {
+
+		params = &ReleasePipelineParams{
+			Prefix:    "mypipeline",
+			Namespace: "default",
+			// @todo: use a valid Pipeline params
+			Extra: ExtraParams{
+				Name:  "ExtraParam",
+				Value: tektonv1beta1.ArrayOrString{Type: tektonv1beta1.ParamTypeString, StringVal: "ExtraValue"},
+			},
+		}
+		releaseParams = &v1alpha1.ReleaseSpec{
+			ApplicationSnapshot: "mysnapshot",
+			ReleaseLink:         "myreleaselink",
+		}
+
+		release = &v1alpha1.Release{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "appstudio.redhat.com/v1alpha1",
+				Kind:       "Release",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "myrelease-",
+				Namespace:    params.Namespace,
+				UID:          "8ba3294a-2e1b-4a13-9b3a-faf3cfe3d31b", // This is an arbitrary random UUID
+			},
+			Spec: *releaseParams,
+		}
+
+		ctx := context.Background()
+
+		// The code below sets the ownership for the Release Object
+		kind := reflect.TypeOf(v1alpha1.Release{}).Name()
+		gvk := v1alpha1.GroupVersion.WithKind(kind)
+		controllerRef := metav1.NewControllerRef(release, gvk)
+
+		// creating a release
+		Expect(k8sClient.Create(ctx, release)).Should(Succeed())
+		release.SetOwnerReferences([]metav1.OwnerReference{*controllerRef})
+
+		// need to set the Kind as it loses it for reasons...
+		release.TypeMeta.APIVersion = "appstudio.redhat.com/v1alpha1"
+		release.TypeMeta.Kind = "Release"
+
+		// Creates the PipelineRun Object
+		rplr = NewReleasePipelineRun(params.Prefix, params.Namespace)
+		Expect(k8sClient.Create(ctx, rplr.AsPipelineRun())).Should(Succeed())
+
+	})
+
+	AfterEach(func() {
+		_ = k8sClient.Delete(ctx, release)
+		_ = k8sClient.Delete(ctx, rplr.AsPipelineRun())
+	})
+
+	Context("Pipeline Run", func() {
+
+		It("Can create a ReleasePipelineRun", func() {
+			Expect(rplr.ObjectMeta.Name).Should(MatchRegexp("mypipeline-" + `[a-z0-9]{5}`))
+			Expect(rplr.ObjectMeta.Namespace).To(Equal("default"))
+		})
+
+		It("Can add extra params to ReleasePipelineRun", func() {
+			rplr.WithExtraParam(params.Extra.Name, params.Extra.Value)
+
+			Expect(rplr.Spec.Params[0].Name).To(Equal("ExtraParam"))
+			Expect(rplr.Spec.Params[0].Value.StringVal).To(Equal("ExtraValue"))
+		})
+
+		It("Can add the release Owner annotations to ReleasePipelineRun", func() {
+			rplr.WithOwner(release)
+			Expect(rplr.Annotations).NotTo(BeNil())
+		})
+
+		It("Can add the release Labels to ReleasePipelineRun", func() {
+			rplr.WithRelease(release)
+			Expect(rplr.Labels["release.appstudio.openshift.io/name"]).
+				Should(MatchRegexp(release.GenerateName +`[a-z1-9]{5}`))
+		})
+
+		It("Can cast ReleasePipelineRun type to PipelineRun", func() {
+			Expect(reflect.TypeOf(rplr.AsPipelineRun())).
+				To(Equal(reflect.TypeOf(&tektonv1beta1.PipelineRun{})))
+		})
+
+	})
+})
---
+++ tekton/tekton_suite_test.go
@@ -0,0 +1,100 @@
+package tekton
+
+import (
+	"context"
+	"go/build"
+	"path/filepath"
+	"testing"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	clientsetscheme "k8s.io/client-go/kubernetes/scheme"
+
+	// "k8s.io/apimachinery/pkg/runtime"
+	appstudiov1alpha1 "github.com/redhat-appstudio/release-service/api/v1alpha1"
+
+	tektonv1beta1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+
+	"k8s.io/client-go/rest"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	//+kubebuilder:scaffold:imports
+)
+
+var (
+	cfg       *rest.Config
+	k8sClient client.Client
+	testEnv   *envtest.Environment
+	ctx       context.Context
+	cancel    context.CancelFunc
+)
+
+func TestPipelineRun(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "PipelineRun Test Suite")
+}
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+	ctx, cancel = context.WithCancel(context.TODO())
+
+	// adding required CRDs, including tekton for PipelineRun Kind
+	testEnv = &envtest.Environment{
+		CRDDirectoryPaths: []string{
+			filepath.Join("..", "config", "crd", "bases"),
+			filepath.Join(
+				build.Default.GOPATH,
+				"pkg", "mod", "github.com", "tektoncd",
+				"pipeline@v0.32.2", "config",
+			),
+		},
+		ErrorIfCRDPathMissing: true,
+	}
+
+	var err error
+	// cfg is defined in this file globally.
+	cfg, err = testEnv.Start()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(cfg).NotTo(BeNil())
+
+	err = appstudiov1alpha1.AddToScheme(clientsetscheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	err = tektonv1beta1.AddToScheme(clientsetscheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	//+kubebuilder:scaffold:scheme
+	k8sClient, err = client.New(cfg, client.Options{
+		Scheme: clientsetscheme.Scheme,
+	})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(k8sClient).NotTo(BeNil())
+
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme: clientsetscheme.Scheme,
+	})
+	Expect(err).NotTo(HaveOccurred())
+
+	err = (&appstudiov1alpha1.Release{}).SetupWebhookWithManager(k8sManager)
+	Expect(err).NotTo(HaveOccurred())
+
+	go func() {
+		defer GinkgoRecover()
+		err = k8sManager.Start(ctx)
+		Expect(err).ToNot(HaveOccurred(), "failed to run manager")
+	}()
+
+}, 60)
+
+var _ = AfterSuite(func() {
+	cancel()
+	By("tearing down the test environment")
+	err := testEnv.Stop()
+	Expect(err).NotTo(HaveOccurred())
+})
