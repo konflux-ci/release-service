1fa071e554fdaa7b8eec67185d076e44ccbc357e
Martin Malina <mmalina@redhat.com>
2023-08-16 11:25:43 +0200

    feat(HACBS-2461): add new pipeline_ref type
    This will be used for enabling git resolvers in release
    strategy CR.
    
    Signed-off-by: Martin Malina <mmalina@redhat.com>

---
+++ tekton/utils/pipeline_ref_test.go
@@ -0,0 +1,103 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Release type", func() {
+	var (
+		clusterRef PipelineRef
+		gitRef     PipelineRef
+		bundleRef  PipelineRef
+	)
+
+	BeforeEach(func() {
+		clusterRef = PipelineRef{
+			Resolver: "cluster",
+			Params: []Param{
+				{Name: "kind", Value: "pipeline"},
+				{Name: "name", Value: "my-cluster-pipeline"},
+				{Name: "namespace", Value: "my-namespace"},
+			},
+		}
+		gitRef = PipelineRef{
+			Resolver: "git",
+			Params: []Param{
+				{Name: "url", Value: "my-git-url"},
+				{Name: "revision", Value: "my-revision"},
+				{Name: "pathInRepo", Value: "my-path-in-repo"},
+			},
+		}
+		bundleRef = PipelineRef{
+			Resolver: "bundles",
+			Params: []Param{
+				{Name: "bundle", Value: "my-bundle"},
+				{Name: "name", Value: "my-pipeline"},
+				{Name: "kind", Value: "Pipeline"},
+			},
+		}
+	})
+
+	When("ToTektonPipelineRef method is called", func() {
+		It("should return Tekton PipelineRef representation of the PipelineRef", func() {
+			ref := clusterRef.ToTektonPipelineRef()
+			Expect(string(ref.ResolverRef.Resolver)).To(Equal("cluster"))
+			params := ref.ResolverRef.Params
+			Expect(params[0].Name).To(Equal("kind"))
+			Expect(params[0].Value.StringVal).To(Equal("pipeline"))
+			Expect(params[1].Name).To(Equal("name"))
+			Expect(params[1].Value.StringVal).To(Equal("my-cluster-pipeline"))
+			Expect(params[2].Name).To(Equal("namespace"))
+			Expect(params[2].Value.StringVal).To(Equal("my-namespace"))
+
+			ref = gitRef.ToTektonPipelineRef()
+			Expect(string(ref.ResolverRef.Resolver)).To(Equal("git"))
+			params = ref.ResolverRef.Params
+			Expect(params[0].Name).To(Equal("url"))
+			Expect(params[0].Value.StringVal).To(Equal("my-git-url"))
+			Expect(params[1].Name).To(Equal("revision"))
+			Expect(params[1].Value.StringVal).To(Equal("my-revision"))
+			Expect(params[2].Name).To(Equal("pathInRepo"))
+			Expect(params[2].Value.StringVal).To(Equal("my-path-in-repo"))
+
+			ref = bundleRef.ToTektonPipelineRef()
+			Expect(string(ref.ResolverRef.Resolver)).To(Equal("bundles"))
+			params = ref.ResolverRef.Params
+			Expect(params[0].Name).To(Equal("bundle"))
+			Expect(params[0].Value.StringVal).To(Equal("my-bundle"))
+			Expect(params[1].Name).To(Equal("name"))
+			Expect(params[1].Value.StringVal).To(Equal("my-pipeline"))
+			Expect(params[2].Name).To(Equal("kind"))
+			Expect(params[2].Value.StringVal).To(Equal("Pipeline"))
+		})
+	})
+
+	When("IsClusterScoped method is called", func() {
+		It("should return true for a cluster pipeline", func() {
+			Expect(clusterRef.IsClusterScoped()).To(BeTrue())
+		})
+
+		It("should return false for non-cluster pipelines", func() {
+			Expect(gitRef.IsClusterScoped()).To(BeFalse())
+			Expect(bundleRef.IsClusterScoped()).To(BeFalse())
+		})
+	})
+
+})
