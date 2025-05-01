0becd2ebb13a28f98f0a498d84df78c06bc647cf
Scott Hebert <scoheb@gmail.com>
2025-05-01 15:40:37 -0400

    feat(RELEASE-1464): update controller rbac
    - need ability to create roles and access secrets
    
    Signed-off-by: Scott Hebert <scoheb@gmail.com>

--- controllers/release/controller.go
+++ controllers/release/controller.go
@@ -53,6 +53,8 @@ type Controller struct {
 //+kubebuilder:rbac:groups=appstudio.redhat.com,resources=releaseserviceconfigs,verbs=get;list;watch
 //+kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch
 //+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=rolebindings,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=roles,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get
 //+kubebuilder:rbac:groups=appstudio.redhat.com,resources=internalrequests,verbs=create;delete;get;list;watch
 //InternalRequests RBAC is required to prevent `forbidden: user system:serviceaccount:release-service:release-service-controller-manager
 //is attempting to grant RBAC permissions not currently held`
